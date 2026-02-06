bffc668c34337b05902c4b045400d2889afa3afc
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2026-02-06 14:00:52 +0530

    Add git-schedule v0.1.0

--- git-schedule.rb
+++ git-schedule.rb
@@ -1,25 +1,20 @@
-# This formula should be placed in your homebrew-tap repository
-# Users install with: brew install yourusername/tap/git-schedule
+# Homebrew formula for git-schedule
+# Users install with: brew install mafex11/tap/git-schedule
 
 class GitSchedule < Formula
   desc "Schedule git commits for later - like delayed send for your code"
-  homepage "https://github.com/mafex11/git-schedule"
+  homepage "https://github.com/mafex11/Git-Schedule"
   version "0.1.0"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/mafex11/git-schedule/releases/download/v#{version}/git-schedule-macos-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    else
-      url "https://github.com/mafex11/git-schedule/releases/download/v#{version}/git-schedule-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    end
+    url "https://github.com/mafex11/Git-Schedule/releases/download/v#{version}/git-schedule-macos-aarch64.tar.gz"
+    sha256 "6c1893d22a9d0bac1a111fe6d11eb487e51228691191eb5aef6b23b0cce16017"
   end
 
   on_linux do
-    url "https://github.com/mafex11/git-schedule/releases/download/v#{version}/git-schedule-linux-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    url "https://github.com/mafex11/Git-Schedule/releases/download/v#{version}/git-schedule-linux-x86_64.tar.gz"
+    sha256 "024287e5d5af4457bc734973aef4667a0c639d4f1f51dbd1c8bb7e9e8b5d27b0"
   end
 
   def install
