0e973188b63ab1b89951a7987eeeb69213e1df4a
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2026-02-06 22:43:22 +0530

    new update checks

--- git-schedule.rb
+++ git-schedule.rb
@@ -4,17 +4,17 @@
 class GitSchedule < Formula
   desc "Schedule git commits for later - like delayed send for your code"
   homepage "https://github.com/mafex11/Git-Schedule"
-  version "0.1.3"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     url "https://github.com/mafex11/Git-Schedule/releases/download/v#{version}/git-schedule-macos-aarch64.tar.gz"
-    sha256 "00861b0a43ae1dc5b857136c7356f385d0a0fabc0d5a89d68be1bc02012d771e"
+    sha256 "89036cb766b4d76f177cd34c6868aa6a12f4ffc786f3b8528caabbe82868eb42"
   end
 
   on_linux do
     url "https://github.com/mafex11/Git-Schedule/releases/download/v#{version}/git-schedule-linux-x86_64.tar.gz"
-    sha256 "3ad9f683b62628791268e32bfb7d9f1b4ae306031c079a3d84765457441be9e9"
+    sha256 "65ea1e437302a3f3981167c788b343e59c7c720ba3a58ac65e276ebbc17a371a"
   end
 
   def install
